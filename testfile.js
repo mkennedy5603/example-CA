65fb3bfd87b5272eb4c9fe612da1348ffd58931b
mkennedy5603 <30702263+mkennedy5603@users.noreply.github.com>
2017-08-03 14:38:39 -0400

    made testfile work with mod 3

--- testfile.js
+++ testfile.js
@@ -3,3 +3,10 @@ var num2 = 3;
 
 console.log(num1 + "+" + num2 + " is ");
 console.log(2+3);
+
+//Now we want to mod by 3.
+
+console.log(num1 + "+" + num2 + " modulo 3 is ");
+console.log((num1 + num 2)%3);
+
+
